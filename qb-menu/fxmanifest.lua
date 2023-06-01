b13dcddb52b2582a5c81563c007053ba0e300426
GenerativeGaming <132309651+Generative-Gaming@users.noreply.github.com>
2023-06-01 19:59:55 +0100

    Add files via upload

---
+++ qb-menu/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'QB-Menu'
+version '1.2.0'
+
+client_script {
+    'client/main.lua',
+    'config.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/script.js',
+    'html/style.css'
+}
+
+lua54 'yes'
